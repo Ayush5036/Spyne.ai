54a26220c94f1e0a143f1ed16700ee071346f744
Ayush5036 <guptaayushcv45@gmail.com>
2024-11-14 23:38:57 +0530

    another 1

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 
   
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
